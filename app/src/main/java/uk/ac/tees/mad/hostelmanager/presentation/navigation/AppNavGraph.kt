c82b5478d267fec7f69c0b1b689fae3bcd5b9d53
Vempatapu Baby Sravani <vbabysravani@gmail.com>
2025-10-30 12:00:00 +0530

    Navigation Integration

---
+++ app/src/main/java/uk/ac/tees/mad/hostelmanager/presentation/navigation/AppNavGraph.kt
@@ -0,0 +1,39 @@
+package uk.ac.tees.mad.hostelmanager.presentation.navigation
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import uk.ac.tees.mad.hostelmanager.presentation.splash.SplashScreen
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
+    object Auth : Screen("auth")
+    object Menu : Screen("menu")
+    object FileComplaint : Screen("file_complaint")
+    object ComplaintStatus : Screen("complaint_status")
+    object MyComplaints : Screen("my_complaints")
+    object Rules : Screen("rules")
+    object Profile : Screen("profile")
+}
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+
+        /*
+        composable(Screen.Auth.route) { AuthScreen(navController) }
+        composable(Screen.Menu.route) { MessMenuScreen(navController) }
+        composable(Screen.FileComplaint.route) { FileComplaintScreen(navController) }
+        composable(Screen.ComplaintStatus.route) { ComplaintStatusScreen(navController) }
+        composable(Screen.MyComplaints.route) { MyComplaintsScreen(navController) }
+        composable(Screen.Rules.route) { HostelRulesScreen(navController) }
+        composable(Screen.Profile.route) { ProfileScreen(navController) }
+        */
+    }
+}
--- app/src/main/java/uk/ac/tees/mad/hostelmanager/ui/theme/Color.kt
+++ app/src/main/java/uk/ac/tees/mad/hostelmanager/ui/theme/Color.kt
@@ -2,6 +2,13 @@ package uk.ac.tees.mad.hostelmanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val PrimaryColor = Color(0xFF006D77)   // Teal Green
+val SecondaryColor = Color(0xFF83C5BE) // Aqua
+val TertiaryColor = Color(0xFFEDF6F9)  // Light background
+val AccentColor = Color(0xFFEE6C4D)    // Warm accent (orange/red)
+val TextPrimary = Color(0xFF1A1A1A)    // Dark text
+val TextSecondary = Color(0xFF555555)  // Muted text
+val BackgroundColor = Color(0xFFF8F9FA) // Almost white background
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
